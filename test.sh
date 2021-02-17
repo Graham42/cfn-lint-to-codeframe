9352591193ca80c7883c8a4f3271527114e3fdf1
Graham McGregor <graham@gmcgregor.ca>
2021-02-17 16:31:30 -0500

    Add test script to check more templates
    This provides a way to check all the current test fixture templates from
    cfn-lint which should cover all the output cases.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# By running
+#
+#   git clone https://github.com/aws-cloudformation/cfn-python-lint.git
+#   find cfn-python-lint/test/fixtures/templates/ -name '*.yaml' -print0 | xargs -0 -P 8 -I{} ./test.sh {}
+#   find cfn-python-lint/test/fixtures/templates/ -name '*.json' -print0 | xargs -0 -P 8 -I{} ./test.sh {}
+#
+# we can find templates that would cause issues.
+
+template="$1"
+input=$(cfn-lint -t "${template}" -f parseable)
+echo "$input" | ./index.js >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Error with $template"
+fi
